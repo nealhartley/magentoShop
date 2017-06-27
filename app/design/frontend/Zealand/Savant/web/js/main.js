9274a3939aa4173d7b0b8e43dc6b9508e31602e8
TheCatTree <steven.c.hartley@gmail.com>
2017-06-27 16:35:41 +1200

    Removed broken module

---
+++ app/design/frontend/Zealand/Savant/web/js/main.js
@@ -0,0 +1,14 @@
+define([
+  "jquery",
+], 
+function($) {
+  "use strict";
+
+  // Here your custom code...
+  console.log('Hola');
+  //$(document).ready(function() {
+	//var address = $(location).attr('href');
+	//console.log(address);
+  //});
+  return;
+});
