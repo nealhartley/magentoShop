4391431717ca70f6a12f0d5932fe19e7f9193de6
TheCatTree <steven.c.hartley@gmail.com>
2017-07-06 21:14:19 +1200

    Working on Jquery Background swap

--- app/design/frontend/Zealand/Savant/web/js/main.js
+++ app/design/frontend/Zealand/Savant/web/js/main.js
@@ -5,10 +5,16 @@ function($) {
   "use strict";
 
   // Here your custom code...
-  console.log('Hola');
-  //$(document).ready(function() {
-	//var address = $(location).attr('href');
-	//console.log(address);
-  //});
+  //console.log('Hola2');
+  $(document).ready(function() {
+	var address = $(location).attr('href');
+	console.log(address);
+	var header = $(".page-header");
+	var me  = document.currentScript;
+	console.log(header.getAttribute('src'));
+	console.log(me.getAttribute('src'));
+	header.css("background-image","none").delay( 5000 ).css("background-image","url(../images/backgrounds/zips.jpg)");
+	console.log('background done');
+  });
   return;
 });
